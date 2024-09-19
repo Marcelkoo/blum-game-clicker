516d3a17ca58655993ed8a3557c628fa09bf3acf
Marcel <107651246+Marcelkoo@users.noreply.github.com>
2024-09-19 16:01:29 +0300

    Update content.js

--- Blum Game Clicker/content.js
+++ Blum Game Clicker/content.js
@@ -16,7 +16,7 @@ function injectScriptWithSettings(file, settings, node) {
 
 chrome.storage.sync.get(['flowerProbability', 'bombProbability', 'iceProbability', 'isScriptEnabled'], function(settings) {
     const loadedSettings = {
-        bombProbability: (settings.bombProbability || 10) / 100,
+        bombProbability: (settings.bombProbability || 3) / 100,
         flowerProbability: (settings.flowerProbability || 80) / 100,
         iceProbability: (settings.iceProbability || 80) / 100,
         isScriptEnabled: settings.isScriptEnabled !== false
