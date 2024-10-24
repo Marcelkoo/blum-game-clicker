3a147e2ed433fd71cbc6ac3a86a88c92bd8e8b3f
Marcel <107651246+Marcelkoo@users.noreply.github.com>
2024-10-24 13:14:05 +0300

    Update popup.js

--- Blum Game Clicker/popup.js
+++ Blum Game Clicker/popup.js
@@ -2,13 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const flowerInput = document.getElementById('flower-probability');
     const bombInput = document.getElementById('bomb-probability');
     const iceInput = document.getElementById('ice-probability');
+    const dogsInput = document.getElementById('dogs-probability');
     const applyButton = document.getElementById('apply-button');
     const enableScriptCheckbox = document.getElementById('enable-script');
   
-    chrome.storage.sync.get(['flowerProbability', 'bombProbability', 'iceProbability', 'isScriptEnabled'], function(settings) {
+    chrome.storage.sync.get(['flowerProbability', 'bombProbability', 'iceProbability', 'dogsProbability', 'isScriptEnabled'], function(settings) {
       flowerInput.value = settings.flowerProbability || 72;
-      bombInput.value = settings.bombProbability || 2;
+      bombInput.value = settings.bombProbability || 1;
       iceInput.value = settings.iceProbability || 80;
+      dogsInput.value = settings.dogsProbability || 99;
       enableScriptCheckbox.checked = settings.isScriptEnabled !== false;
     });
   
@@ -16,18 +18,21 @@ document.addEventListener('DOMContentLoaded', function() {
       const flowerProbability = parseFloat(flowerInput.value);
       const bombProbability = parseFloat(bombInput.value);
       const iceProbability = parseFloat(iceInput.value);
+      const dogsProbability = parseFloat(dogsInput.value);
       const isScriptEnabled = enableScriptCheckbox.checked;
   
       chrome.storage.sync.set({
         flowerProbability: flowerProbability,
         bombProbability: bombProbability,
         iceProbability: iceProbability,
+        dogsProbability: dogsProbability,
         isScriptEnabled: isScriptEnabled
       }, function() {
         chrome.runtime.sendMessage({
           flowerProbability: flowerProbability,
           bombProbability: bombProbability,
           iceProbability: iceProbability,
+          dogsProbability: dogsProbability,
           isScriptEnabled: isScriptEnabled
         });
   
