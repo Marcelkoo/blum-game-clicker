30c55ead290715a5742a4f5c4d9819796efdbf78
Marcel <107651246+Marcelkoo@users.noreply.github.com>
2024-11-05 13:11:25 +0300

    Update popup.js

--- Blum Game Clicker/popup.js
+++ Blum Game Clicker/popup.js
@@ -2,15 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const flowerInput = document.getElementById('flower-probability');
     const bombInput = document.getElementById('bomb-probability');
     const iceInput = document.getElementById('ice-probability');
-    const dogsInput = document.getElementById('dogs-probability');
+    const trumpInput = document.getElementById('trump-probability');
+    const harrisInput = document.getElementById('harris-probability');
     const applyButton = document.getElementById('apply-button');
     const enableScriptCheckbox = document.getElementById('enable-script');
   
-    chrome.storage.sync.get(['flowerProbability', 'bombProbability', 'iceProbability', 'dogsProbability', 'isScriptEnabled'], function(settings) {
+    chrome.storage.sync.get(['flowerProbability', 'bombProbability', 'iceProbability', 'trumpProbability', 'harrisProbability', 'isScriptEnabled'], function(settings) {
       flowerInput.value = settings.flowerProbability || 72;
       bombInput.value = settings.bombProbability || 1;
       iceInput.value = settings.iceProbability || 80;
-      dogsInput.value = settings.dogsProbability || 99;
+      trumpInput.value = settings.trumpProbability || 99;
+      harrisInput.value = settings.harrisProbability || 99;
       enableScriptCheckbox.checked = settings.isScriptEnabled !== false;
     });
   
@@ -18,21 +20,24 @@ document.addEventListener('DOMContentLoaded', function() {
       const flowerProbability = parseFloat(flowerInput.value);
       const bombProbability = parseFloat(bombInput.value);
       const iceProbability = parseFloat(iceInput.value);
-      const dogsProbability = parseFloat(dogsInput.value);
+      const trumpProbability = parseFloat(trumpInput.value);
+      const harrisProbability = parseFloat(harrisInput.value);
       const isScriptEnabled = enableScriptCheckbox.checked;
   
       chrome.storage.sync.set({
         flowerProbability: flowerProbability,
         bombProbability: bombProbability,
         iceProbability: iceProbability,
-        dogsProbability: dogsProbability,
+        trumpProbability: trumpProbability,
+        harrisProbability: harrisProbability,
         isScriptEnabled: isScriptEnabled
       }, function() {
         chrome.runtime.sendMessage({
           flowerProbability: flowerProbability,
           bombProbability: bombProbability,
           iceProbability: iceProbability,
-          dogsProbability: dogsProbability,
+          trumpProbability: trumpProbability,
+          harrisProbability: harrisProbability,
           isScriptEnabled: isScriptEnabled
         });
   
