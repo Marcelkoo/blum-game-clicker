46d9f4733a15868882f4ed339aaab94e24f7ee57
Marcel <107651246+Marcelkoo@users.noreply.github.com>
2024-11-07 10:37:37 +0300

    Update popup.js

--- Blum Game Clicker/popup.js
+++ Blum Game Clicker/popup.js
@@ -2,8 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const flowerInput = document.getElementById('flower-probability');
     const bombInput = document.getElementById('bomb-probability');
     const iceInput = document.getElementById('ice-probability');
-    const trumpInput = document.getElementById('trump-probability');
-    const harrisInput = document.getElementById('harris-probability');
     const applyButton = document.getElementById('apply-button');
     const enableScriptCheckbox = document.getElementById('enable-script');
   
@@ -11,8 +9,6 @@ document.addEventListener('DOMContentLoaded', function() {
       flowerInput.value = settings.flowerProbability || 72;
       bombInput.value = settings.bombProbability || 1;
       iceInput.value = settings.iceProbability || 80;
-      trumpInput.value = settings.trumpProbability || 99;
-      harrisInput.value = settings.harrisProbability || 99;
       enableScriptCheckbox.checked = settings.isScriptEnabled !== false;
     });
   
@@ -20,24 +16,18 @@ document.addEventListener('DOMContentLoaded', function() {
       const flowerProbability = parseFloat(flowerInput.value);
       const bombProbability = parseFloat(bombInput.value);
       const iceProbability = parseFloat(iceInput.value);
-      const trumpProbability = parseFloat(trumpInput.value);
-      const harrisProbability = parseFloat(harrisInput.value);
       const isScriptEnabled = enableScriptCheckbox.checked;
   
       chrome.storage.sync.set({
         flowerProbability: flowerProbability,
         bombProbability: bombProbability,
         iceProbability: iceProbability,
-        trumpProbability: trumpProbability,
-        harrisProbability: harrisProbability,
         isScriptEnabled: isScriptEnabled
       }, function() {
         chrome.runtime.sendMessage({
           flowerProbability: flowerProbability,
           bombProbability: bombProbability,
           iceProbability: iceProbability,
-          trumpProbability: trumpProbability,
-          harrisProbability: harrisProbability,
           isScriptEnabled: isScriptEnabled
         });
   
